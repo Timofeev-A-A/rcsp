d9d309eef2d117a825f1b45798be888bd4d40286
Hawk <tsasha1208@rambler.ru>
2022-09-21 13:27:32 +0300

    Task two done

--- src/FirstPractice/TaskTwo.java
+++ src/FirstPractice/TaskTwo.java
@@ -1,4 +1,40 @@
 package FirstPractice;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class TaskTwo {
+    public static class Calculator{
+        private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        public Future<Integer> calculate(Integer input) {
+            return executor.submit(() -> {
+                int timer = (int) Math.round(1 + (Math.random() * 4));
+                System.out.println("Task will be solved in " + timer + " seconds");
+                Thread.sleep(timer * 1000);
+                return input * input;
+            });
+        }
+    }
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        while (true) {
+            int num = scanner.nextInt();
+            Future<Integer> future = calculator.calculate(num);
+            while (!future.isDone()) {
+                Thread.sleep(100);
+            }
+            Integer result = future.get();
+            System.out.println(result);
+            if (result == 0)
+                break;
+        }
+        calculator.executor.shutdown();
+    }
 }
